1e424db5b37ad2dd798befa115608e646af0caaa
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-10-08 07:36:37 +0530

    add design

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/Home.css'; // Custom CSS file for styling
+import Dashboard from "../Images/dashboard.png"
+import Dash2 from "../Images/Dash2.png"
 
 
 function Home() {
   return (
-    <div className="home-container">
-     
+    <div className="">
+    <div className='flex mt-32 mb-32'>
+    <div>
+    <img src={Dash2} alt="Dashboard" className="w-[500px] h-auto rounded-xl" />
+    </div>
+    <div>
+    <img src={Dashboard} alt="Dashboard" className="w-[500px] h-auto rounded-xl" />
+    
+    </div>
+    
+</div>
+
+
     </div>
   );
 }
